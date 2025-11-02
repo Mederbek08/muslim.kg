43de1fcc0f59edb9ca079e2d4cb639b53ff212a9
Mederbek <mederbekrahmatullaev7@gmail.com>
2025-11-02 22:44:49 +0600

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Router from "./router";
 import CartModal from "./components/CartModal";
 import { CartProvider } from "./components/CartContext";
 import CustomCursor from "./components/CustomCursor";
+import { fetchData } from './api';
 
 const App = () => {
+  useEffect(() => {
+    fetchData().then(data => console.log(data));
+  }, []);
+
   return (
-    <>
-      <CartProvider>
-        <Router /> <CartModal /> <CustomCursor />
-      </CartProvider>
-    </>
+    <CartProvider>
+      <Router />
+      <CartModal />
+      <CustomCursor />
+    </CartProvider>
   );
 };
 
---
+++ src/api.js
@@ -0,0 +1,9 @@
+// src/api.js
+const apiKey = import.meta.env.VITE_API_KEY;
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export async function fetchData() {
+  const res = await fetch(`${apiUrl}/data?key=${apiKey}`);
+  const data = await res.json();
+  return data;
+}
